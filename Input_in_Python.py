b363309729a02239f017c7538877d867f280cab9
Nihal Ali <nihalali419@gmail.com>
2023-06-12 23:09:00 +0500

    Taking input in Python

---
+++ Input_in_Python.py
@@ -0,0 +1,19 @@
+a = input("Enter your name: ")
+print("Name is:", a)
+
+b = input("Enter first number: ")  # takes input as String, thus this will concatenate when added.
+
+c = int(input("Enter second number: "))
+d = int(input("Enter third number: "))
+print(c + d)
+
+# OR
+e = input("Enter fourth number: ")
+f = input("Enter fifth number: ")
+
+print(int(e) + int(f))
+
+print(int(e) / int(f))
+print(int(e) % int(f))
+print(int(e) ** int(f))
+print(int(e) // int(f))
